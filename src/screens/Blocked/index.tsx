5dbe93852b942e608e7845959fa5ec4ef4fac6fd
Matheus Frej <matheusfrej@hotmail.com>
2023-11-18 16:01:19 -0300

    Tela de bloqueados

--- src/screens/Blocked/index.tsx
+++ src/screens/Blocked/index.tsx
@@ -1,13 +1,39 @@
 import { BackButton } from '@components/BackButton'
 import { ScrollContainer } from '../../components/ScrollContainer'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as S from './styles'
 import { FriendCard } from '@components/FriendCard'
 import { NoContentText } from '@components/NoContentText'
+import { UserDTO } from '../../dtos/userDTO'
+import { useAuth } from '../../contexts/AuthContext'
+import api from '../../libs/api'
+import { AppError } from '@utils/AppError'
 
 export function Blocked() {
-  const [areThereBlocked, setAreThereBlocked] = useState(false)
+  const { setSnackbarStatus } = useAuth()
+  const [blocked, setBlocked] = useState<UserDTO[]>([])
+
+  const areThereBlocked = blocked.length > 0
+
+  useEffect(() => {
+    const fetchBlocked = async () => {
+      try {
+        const response = await api.get('/blocks')
+
+        setBlocked(response.data.blocks)
+      } catch (error) {
+        const isAppError = error instanceof AppError
+
+        const title = isAppError
+          ? error.message
+          : 'Não foi possível obter os usuários bloqueados. Tente novamente mais tarde.'
+        setSnackbarStatus(title, false)
+      }
+    }
+
+    if (blocked.length === 0) fetchBlocked()
+  }, [])
 
   return (
     <ScrollContainer>
@@ -16,12 +42,12 @@ export function Blocked() {
         <S.Title>Bloqueados</S.Title>
         {areThereBlocked ? (
           <S.BlockedContainer>
-            <FriendCard name="Bruna" />
-            <FriendCard name="Matheus" />
-            <FriendCard name="Zé" />
+            {blocked.map((user) => {
+              return <FriendCard key={user.id} user={user} />
+            })}
           </S.BlockedContainer>
         ) : (
-          <NoContentText text="Nenhum usuário bloquado no momento." />
+          <NoContentText text="Nenhum usuário bloqueado no momento." />
         )}
       </S.Container>
     </ScrollContainer>
